10399520d673635344b57b5f2ed1a7ca8d6e9953
Alex Kestner <akestner@users.noreply.github.com>
2014-05-30 22:57:02 -0400

    added name to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
-maintainer       "Damon Jablons"
-maintainer_email "damonjablons@gmail.com"
+name             "virtualenvwrapper"
+maintainer       "Alex Kestner"
+maintainer_email "akestner@macroeyes.com"
 license          "Apache 2.0"
 description      "Installs/Configures virtualenvwrapper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 depends "python"
