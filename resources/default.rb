8ce84675689d9791135a08e8af585d4c62b6d311
Alex Kestner <akestner@users.noreply.github.com>
2014-05-30 22:51:43 -0400

    Updated resource/default.rb with environments attr

--- resources/default.rb
+++ resources/default.rb
@@ -27,5 +27,6 @@ end
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :owner, :kind_of => String
 attribute :group, :kind_of => String
+attribute :environment, :kind_of => Hash
 attribute :requirements, :kind_of => String
 attribute :packages, :kind_of => Array
